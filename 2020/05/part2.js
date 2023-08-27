31614c286dd4d57dfc000654de104655c501441b
Trevon Romanuik <trevon.romanuik@gmail.com>
2023-08-27 10:27:37 -0600

    2020/05

---
+++ 2020/05/part2.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+
+const test = false;
+const debug = true;
+const input_file = test ? './test_input.txt' : './input.txt';
+const input = fs.readFileSync(path.resolve(__dirname, input_file), 'utf-8');
+
+function bsplit(a) {
+  if(a.length === 2) return a;
+  return [
+    bsplit(a.slice(0, a.length / 2)),
+    bsplit(a.slice(a.length / 2)),
+  ];
+}
+
+function btree(d) {
+  const n = Math.pow(2, d);
+  const a = new Array(n);
+  for(let i = 0; i < n; i++) {
+    a[i] = i;
+  }
+  return bsplit(a);
+}
+
+let min = Infinity;
+let max = -Infinity;
+const sids = new Set();
+const rtree = btree(7);
+const ctree = btree(3);
+input.split('\r\n').forEach((line) => {
+  let r = rtree;
+  for(let i = 0; i < 7; i++) {
+    r = r[line[i] === 'F' ? 0 : 1];
+  }
+  let c = ctree;
+  for(let i = 7; i < 10; i++) {
+    c = c[line[i] === 'L' ? 0 : 1];
+  }
+  const s = r * 8 + c;
+  console.log({ line, r, c, s });
+  sids.add(s);
+  if(s < min) min = s;
+  if(s > max) max = s;
+});
+console.log({ min, max });
+for(let i = min + 1; i < max; i++) {
+  if(sids.has(i - 1) && !sids.has(i) && sids.has(i + 1)) {
+    console.log(i);
+    process.exit(0);
+  }
+}
