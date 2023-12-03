69681ebee00a23bd4602f129ae049e57a56f3239
Trevon Romanuik <trevon.romanuik@gmail.com>
2023-12-03 09:37:49 -0700

    added __template dir to copy

---
+++ __template/part_2.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+const test = true;
+const debug = true;
+const input_file = test ? './test_input.txt' : './input.txt';
+const input = fs.readFileSync(path.resolve(__dirname, input_file), 'utf-8');
