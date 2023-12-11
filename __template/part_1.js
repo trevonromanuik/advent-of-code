7d20109cebb14546bfaa109ed8016aafc974814f
Trevon Romanuik <trevon.romanuik@gmail.com>
2023-12-11 08:44:11 -0700

    fixed timing log in template

--- __template/part_1.js
+++ __template/part_1.js
@@ -8,7 +8,7 @@ const input = fs.readFileSync(path.resolve(__dirname, input_file), 'utf-8');
 
 function hrTime() {
     const t = process.hrtime();
-    return t[0] * 1000000 + t[1] / 1000;
+    return Math.floor(t[0] * 1000000 + t[1] / 1000);
 }
 
 const start = hrTime();
