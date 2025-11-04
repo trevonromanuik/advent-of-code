e621a02ab3c5e51bae8103fe4904178d2713e4bc
trevonromanuik <trevon.romanuik@gmail.com>
2025-11-04 09:06:45 -0700

    2015/17

---
+++ 2015/17/part_2.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+const test = false;
+const debug = true;
+const input_file = test ? './test_input.txt' : './input.txt';
+const input = fs.readFileSync(path.resolve(__dirname, input_file), 'utf-8');
+
+function hrTime() {
+  const t = process.hrtime();
+  return Math.floor(t[0] * 1000000 + t[1] / 1000);
+}
+
+const start = hrTime();
+
+const LITERS = test ? 25 : 150;
+const buckets = input.replaceAll('\r', '').split('\n').map((line) => {
+  return parseInt(line);
+}).sort((a, b) => {
+  return a < b ? 1 : -1;
+});
+
+let count = 0;
+let min_length = Infinity;
+const q = buckets.map((n, i) => {
+  return [n, i, [n]];
+});
+while (q.length) {
+  const [n, i, p] = q.pop();
+  for (let j = i + 1; j < buckets.length; j++) {
+    const new_n = n + buckets[j];
+    const new_p = [...p, buckets[j]];
+    if (new_n === LITERS) {
+      if (new_p.length < min_length) {
+        min_length = new_p.length;
+        count = 0;
+      }
+      if (new_p.length === min_length) {
+        count++;
+      }
+    } else if (new_n < LITERS) {
+      q.push([new_n, j, new_p]);
+    }
+  }
+}
+console.log({ count });
+
+console.log(`${hrTime() - start}µs`);
