f2f0d0ef08658c0cb225e553e20aba69633032f9
trevonromanuik <trevon.romanuik@gmail.com>
2025-11-05 09:01:20 -0700

    2015/18

---
+++ 2015/18/part_1.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+
+const test = false;
+const debug = true;
+const input_file = test ? './test_input.txt' : './input.txt';
+const input = fs.readFileSync(path.resolve(__dirname, input_file), 'utf-8');
+
+function hrTime() {
+    const t = process.hrtime();
+    return Math.floor(t[0] * 1000000 + t[1] / 1000);
+}
+
+const start = hrTime();
+
+let map = input.replaceAll('\r', '').split('\n').map((line) => {
+  return line.split('').map((c) => {
+    return c === '#';
+  });
+});
+
+function printMap(map) {
+  console.log(map.map((row) => {
+    return row.map((b) => {
+      return b ? '#' : '.';
+    }).join('');
+  }).join('\n'));
+}
+
+// console.log(`initial state:`);
+// printMap(map);
+
+const HEIGHT = map.length;
+const WIDTH = map[0].length;
+
+const STEPS = test ? 4 : 100;
+
+for (let i = 0; i < STEPS; i++) {
+  const new_map = new Array(HEIGHT);
+  for (let y = 0; y < HEIGHT; y++) {
+    new_map[y] = new Array(WIDTH);
+    for (let x = 0; x < WIDTH; x++) {
+      let count = 0;
+      for (let dy = -1; dy <= 1; dy++) {
+        for (let dx = -1; dx <= 1; dx++) {
+          if (dy === 0 && dx === 0) continue;
+          const ny = y + dy;
+          const nx = x + dx;
+          if (map[ny]?.[nx]) count++;
+        }
+      }
+      new_map[y][x] = map[y][x] ?
+        (count === 2 || count === 3) :
+        (count === 3);
+    }
+  }
+  map = new_map;
+  // console.log(`\nafter ${i + 1} steps:`);
+  // printMap(map);
+}
+
+const count = map.reduce((count, row) => {
+  return count + row.reduce((count, b) => {
+    return count + (b ? 1 : 0);
+  }, 0);
+}, 0);
+console.log({ count });
+
+console.log(`${hrTime() - start}µs`);
