d3273be72f2a26096c5de3a6d314a86d8c69889c
trevonromanuik <trevon.romanuik@gmail.com>
2025-11-07 14:46:11 -0700

    2015/20

---
+++ 2015/20/part_2.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+const test = false;
+const debug = true;
+const input_file = test ? './test_input.txt' : './input.txt';
+const input = fs.readFileSync(path.resolve(__dirname, input_file), 'utf-8');
+
+function hrTime() {
+  const t = process.hrtime();
+  return Math.floor(t[0] * 1000000 + t[1] / 1000);
+}
+
+const start = hrTime();
+
+const target = parseInt(input);
+
+function countPresents(house_number) {
+  let count = 0;
+  for (let i = 1; i <= house_number; i++) {
+    if (house_number % i === 0) {
+      if (house_number / i < 50) {
+        count += (i * 11);
+      }
+    }
+  }
+  return count;
+}
+
+const START_HOUSE_NUMBER = 800000;
+const HOUSES_COUNT = 100000;
+for (let house_number = START_HOUSE_NUMBER; house_number < START_HOUSE_NUMBER + HOUSES_COUNT; house_number++) {
+  const count = countPresents(house_number);
+  if (count >= target) {
+    console.log(`house ${house_number} got ${count} presents`);
+    break;
+  }
+}
+
+console.log(`${hrTime() - start}µs`);
