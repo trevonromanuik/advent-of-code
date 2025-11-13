394264da97c303842abdd3e08f7786c59d47b5a6
trevonromanuik <trevon.romanuik@gmail.com>
2025-11-13 09:29:13 -0700

    2016/09

---
+++ 2016/09/part_2.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const test = false;
+const debug = true;
+const input_file = test ? './test_input.txt' : './input.txt';
+const input = fs.readFileSync(path.resolve(__dirname, input_file), 'utf-8');
+
+function hrTime() {
+  const t = process.hrtime();
+  return Math.floor(t[0] * 1000000 + t[1] / 1000);
+}
+
+const start = hrTime();
+
+function countLength(s) {
+  if (test) console.log(`countLength(${s})`);
+  let length = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      let j = i + 1;
+      for (j; j < s.length; j++) {
+        if (s[j] === ')') {
+          const [n, r] = s.substring(i + 1, j).split('x').map(n => parseInt(n));
+          const l = countLength(s.substring(j + 1, j + 1 + n));
+          length += (l * r);
+          i = j + n;
+          break;
+        }
+      }
+    } else {
+      length += 1;
+    }
+  }
+  return length;
+}
+
+const length = countLength(input);
+
+console.log({ length });
+
+console.log(`${hrTime() - start}µs`);
