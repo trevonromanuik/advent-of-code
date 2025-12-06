84c0e75bb042af6cb75f5eb5b3f9c20af5110cf3
trevonromanuik <trevon.romanuik@gmail.com>
2025-12-06 07:15:48 -0700

    2025/05

---
+++ 2025/05/part_2.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+
+const test = false;
+const debug = true;
+const input_file = test ? './test_input.txt' : './input.txt';
+const input = fs.readFileSync(path.resolve(__dirname, input_file), 'utf-8');
+
+function summary(start) {
+  const time = `${hrTime() - start}µs`;
+  const usage = memoryUsage();
+  usage['time'] = time;
+  return usage;
+}
+
+function memoryUsage() {
+  const usage = process.memoryUsage();
+  Object.keys(usage).forEach((key) => {
+    usage[key] = `${Math.round(usage[key] / 1024 / 1024 * 100) / 100} MB`
+  });
+  return usage;
+}
+
+function hrTime() {
+  const t = process.hrtime();
+  return Math.floor(t[0] * 1000000 + t[1] / 1000);
+}
+
+const start = hrTime();
+
+const [ranges_chunk,] = input.replaceAll('\r', '').split('\n\n');
+const raw_ranges = ranges_chunk.split('\n').map((line) => {
+  return line.split('-').map(n => parseInt(n));
+});
+raw_ranges.sort((a, b) => {
+  return a[0] === b[0] ?
+    a[1] < b[1] ? -1 : 1 :
+    a[0] < b[0] ? -1 : 1;
+});
+const ranges = raw_ranges.reduce((ranges, range) => {
+  if (!ranges.length) {
+    ranges.push(range);
+  } else {
+    const prev_range = ranges[ranges.length - 1];
+    if (range[0] <= prev_range[1]) {
+      if (range[1] > prev_range[1]) {
+        prev_range[1] = range[1];
+      }
+    } else {
+      ranges.push(range);
+    }
+  }
+  return ranges;
+}, []);
+
+const count = ranges.reduce((count, range) => {
+  return count + (range[1] - range[0] + 1);
+}, 0);
+
+console.log({ count });
+
+console.log(JSON.stringify(summary(start)));
